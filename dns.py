1ba1fe75b43354ed805d3e5fe5118fce948b4f2f
ashtnemi448 <atharwaadawadkar448@gmail.com>
2020-04-07 19:03:09 +0530

    Create dns.py

---
+++ dns.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2
+'''
+import netfilterqueue
+import scapy.all as scapy
+import os
+'''
+
+import subprocess
+import sys
+
+def install(package):
+    pip.main(['install', package])
+
+def process_packet(packet):
+
+	scapy_packet = scapy.IP(packet.get_payload())
+	if scapy_packet.haslayer(scapy.DNSRR):
+		qname = scapy_packet[scapy.DNSQR].qname
+		print(qname)
+		f = open("visited.txt", "a")		
+		f.write(qname)
+		f.close()
+
+	packet.accept()
+
+
+try:
+    import scapy.all as scapy
+except ImportError:
+    subprocess.call([sys.executable, "-m", "pip2", "install", 'scapy'])
+finally:
+    import scapy.all as scapy
+
+try:
+    import os
+except ImportError:
+    subprocess.call([sys.executable, "-m", "pip2", "install", 'os'])
+finally:
+    import os
+
+try:
+    import netfilterqueue
+except ImportError:
+    subprocess.call([sys.executable, "-m", "pip2", "install", 'netfilterqueue'])
+finally:
+    import netfilterqueue
+
+
+os.system("iptables --flush")
+os.system("iptables -I INPUT  -j NFQUEUE --queue-num 1")
+os.system("iptables -I OUTPUT  -j NFQUEUE --queue-num 1")
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(1,process_packet)
+queue.run()
